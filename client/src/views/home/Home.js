e8291eca7ba5af1a4910aba0866ebb0a88186761
Audun Wigum Arbo <audunwar@stud.ntnu.no>
2018-07-07 16:19:29 +0200

    Add new home screen text

--- client/src/views/home/Home.js
+++ client/src/views/home/Home.js
@@ -13,13 +13,13 @@ function onSearchClick(e) {
 }
 var Home = {
     view: function () {
-        return m(".center-align",
+        return m(".center-align.container",
             [
                 m("h1", "VinDB"),
                 m("h5",
                     [
-                        m("p", "VinDB gir deg oversikt over alle varene Vinmonopolet tilbyr, og hvilke endringer som blir gjort på disse hver dag."),
-                        m("p", "Du kan dermed få med deg alt fra prisendringer til rettinger av skrivefeil!")
+                        m("p", "VinDB er en tjeneste som tilbyr oversikt over alle endringer gjort i Vinmonopolets vareutvalg. Vi sjekker daglig hvilke endringer Vinmonopolet gjør, og man kan dermed se alt fra prisendringer til rettinger av skrivefeil!\n"),
+                        m("p", "Skriv i søkefeltet, eller velg en kategori fra toppmenyen!")
                     ]
                 ),
                 m("form[action='/products'][id='searchForm']",
