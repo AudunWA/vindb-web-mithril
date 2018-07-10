1ab3a92414b9d435d603cd82aa1176effdf86601
Audun Wigum Arbo <audunwar@stud.ntnu.no>
2018-07-10 21:33:38 +0200

    Add Google Analytics and Ads

--- client/src/index.js
+++ client/src/index.js
@@ -12,26 +12,52 @@ m.route.prefix("");
 m.route(document.body, "/", {
     "/": {
         render: function () {
+            ga('set', 'page', '/');
+            ga('send', 'pageview');
             return m(Layout, m(Home));
         }
     },
     "/products": {
         render: function () {
+            ga('set', 'page', '/products');
+            ga('send', 'pageview');
             return m(Layout, m(ProductList));
         }
     }
     ,
     "/product/:id/:tab": {
         render: function (vnode) {
+            ga('set', 'page', `/product/${vnode.attrs.id}/${vnode.attrs.tab}`);
+            ga('send', 'pageview');
             return m(Product, vnode.attrs);
         }
     },
     "/product/:id": {
         render: function (vnode) {
+            ga('set', 'page', `/product/${vnode.attrs.id}`);
+            ga('send', 'pageview');
             return m(Product, vnode.attrs);
         }
     },
-    "/pricechanges": PriceChanges,
-    "/pricechangesold": PriceChangesOld,
-    "/history": AllChanges
+    "/pricechanges": {
+        render: function (vnode) {
+            ga('set', 'page', '/pricechanges');
+            ga('send', 'pageview');
+            return m(PriceChanges, vnode.attrs);
+        }
+    },
+    "/pricechangesold": {
+        render: function (vnode) {
+            ga('set', 'page', '/pricechangesold');
+            ga('send', 'pageview');
+            return m(PriceChangesOld, vnode.attrs);
+        }
+    },
+    "/history": {
+        render: function (vnode) {
+            ga('set', 'page', '/history');
+            ga('send', 'pageview');
+            return m(AllChanges, vnode.attrs);
+        }
+    },
 });
